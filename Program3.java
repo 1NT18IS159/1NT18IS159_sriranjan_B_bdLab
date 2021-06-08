9959373bc0f008bdf30049990cb17b8bf66e7c8d
1NT18IS159 <82133887+1NT18IS159@users.noreply.github.com>
2021-06-08 22:06:49 +0530

    Salary = 30,000
    And the number of awards they have won!

---
+++ Program3.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+// Total number of awards for those with salary = 30000
+
+public class Program3 {
+
+	//MAPPER CODE	
+		   
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+	
+	// the variable one holds the value 1 in the form of Hadoop's IntWritable Object
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+		String myvalue = value.toString();
+		String[] tokens = myvalue.split(",");
+		Text award = new Text("Awards equal to 30k: ");
+		if (Integer.parseInt(tokens[2]) == 30000) {
+			output.collect(award, new IntWritable(Integer.parseInt(tokens[3])));
+		}
+		}
+	}
+
+	//REDUCER CODE	
+	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException { 
+			int transcount = 0;
+			while(values.hasNext()) {
+				transcount += values.next().get();
+			}
+			
+			output.collect(new Text("Awards equal to 30k: "), new IntWritable(transcount));
+		}
+	}
+		
+	//DRIVER CODE
+	public static void main(String[] args) throws Exception {
+		
+		// JobConf is a constructor and is used to construct a map/reduce job configuration.
+		JobConf conf = new JobConf(Program3.class);
+		conf.setJobName("Various Operations");
+		
+		//Set the key class for the job output data.
+		// Text class is part of hadoop API
+		conf.setOutputKeyClass(Text.class);
+		
+		// Set the value class for job outputs 
+		conf.setOutputValueClass(IntWritable.class);
+		
+		// set the mapper class
+		conf.setMapperClass(Map.class);
+		
+		// set the combiner class
+		conf.setCombinerClass(Reduce.class);
+		
+		// set the reducer class
+		conf.setReducerClass(Reduce.class);
+		
+		// set input format
+		conf.setInputFormat(TextInputFormat.class);
+		
+		//set output format
+		conf.setOutputFormat(TextOutputFormat.class); 
+
+		// set the first argument as input
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		
+		//set the second argument as output
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		
+		// run the job
+		JobClient.runJob(conf);   
+	}
+}
